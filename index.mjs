5bde7b6a248cfdbbf7be084ad4a5ea56ab656568
skyboooox <zcj924@gmail.com>
2024-08-03 01:27:23 +0800

    -

---
+++ index.mjs
@@ -0,0 +1,93 @@
+import { EventEmitter } from "events";
+export const event = new EventEmitter();
+
+import { customAlphabet } from "nanoid";
+export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXY", 12);
+
+/**
+ * @param {number} ms
+ */
+export async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * @param {string} url
+ * @param {number} timeout
+ */
+export async function fetchWithTimeout(url = '/', timeout = 5000) {
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const response = await fetch(url, { signal });
+    clearTimeout(timeoutId);
+    return response;
+  } catch (error) {
+    if (error.name === 'AbortError') {
+      console.warn('Request timed out');
+      return null;
+    }
+    throw error;
+  }
+}
+
+/**
+ * @param {Boolean} float 
+ */
+export function uptime(float = false) {
+  if (float) return process.uptime() * 100000
+  return Math.round(process.uptime() * 100000);
+}
+
+export function loop(tps = 60) {
+  let tickDuration = 1000 / tps;
+  let lastTickTime = Date.now();
+  let callbacks = [];
+  let isRunning = false;
+  let warningCounter = 0;
+
+  async function loop() {
+    if (!isRunning) return;
+
+    const currentTime = Date.now();
+    const deltaTime = currentTime - lastTickTime;
+
+    if (deltaTime >= tickDuration) {
+      lastTickTime = currentTime + (deltaTime % tickDuration);
+      const start = Date.now();
+
+      await Promise.all(callbacks.map(callback => callback(currentTime)));
+
+      const processingTime = Date.now() - start;
+
+      if (processingTime > tickDuration) {
+        warningCounter++;
+        if (warningCounter >= 5) {
+          console.warn(`Can't keep up! This tick took ${processingTime}ms longer than ${tickDuration.toFixed(2)}ms`);
+          warningCounter = 0;
+        }
+      } else {
+        warningCounter = 0;
+      }
+    }
+
+    setTimeout(loop, tickDuration - (Date.now() - currentTime));
+  }
+
+  function start() {
+    if (!isRunning) {
+      isRunning = true;
+      loop();
+    }
+  }
+
+  function funcLoop(callback) {
+    callbacks.push(callback);
+    start();
+  }
+
+  return funcLoop;
+}
