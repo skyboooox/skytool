7a7d614841df64ad2591dada7e2640d8766c06ff
skyboooox <zcj924@gmail.com>
2024-09-13 22:36:28 +0800

    modified:   index.mjs

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXY", 12);
 
 /**
  * @param {number} ms
+ * @returns {Promise<void>} Promise
  */
 export async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -14,6 +15,7 @@ export async function delay(ms) {
 /**
  * @param {string} url
  * @param {number} timeout
+ * @returns {Promise<Response>} Response
  */
 export async function fetchWithTimeout(url = '/', timeout = 5000) {
   const controller = new AbortController();
@@ -30,18 +32,23 @@ export async function fetchWithTimeout(url = '/', timeout = 5000) {
       console.warn('Request timed out');
       return null;
     }
-    throw error;
+    throw error; 
   }
 }
 
 /**
- * @param {Boolean} float 
+ * @param {Boolean} float Node only
+ * @returns {number} Uptime
  */
 export function uptime(float = false) {
-  if (float) return process.uptime() * 100000
+  if (float) return process.uptime()
   return Math.round(process.uptime() * 100000);
 }
 
+/**
+ * @param {number} tps Ticks per second
+ * @returns {Function} Loop function
+ */
 export function loop(tps = 60) {
   let tickDuration = 1000 / tps;
   let lastTickTime = Date.now();
@@ -90,4 +97,27 @@ export function loop(tps = 60) {
   }
 
   return funcLoop;
+}
+
+/**
+ * @param {string} splitter Hash splitter Default: &
+ * @returns {Object} Hash
+ */
+
+export function getHash(splitter) {
+  let hash = window.location.hash;
+  if (!hash) return null;
+  hash = decodeURIComponent(hash);
+  hash = hash.replace('#', '').split(splitter || '&');
+  let obj = {};
+  for (var kv = 0; kv < hash.length; kv++) {
+      var flag = hash[kv].split("=");
+      if (flag.length == 1) {
+          obj[flag[0]] = true;
+      } else {
+          obj[flag[0]] = flag[1];
+      }
+  }
+  return obj;
+
 }
