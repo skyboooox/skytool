5861f0dcf15e9645d94478edbe23f9a277e3caf1
skyboooox <20927006+skyboooox@users.noreply.github.com>
2025-06-19 13:36:33 +0800

    modified:   index.mjs

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXY", 12);
  * @returns {Promise<void>} Promise
  */
 export async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
@@ -17,7 +17,7 @@ export async function delay(ms) {
  * @param {number} timeout
  * @returns {Promise<Response>} Response
  */
-export async function fetchWithTimeout(url = '/', timeout = 5000) {
+export async function fetchWithTimeout(url = "/", timeout = 5000) {
   const controller = new AbortController();
   const { signal } = controller;
 
@@ -28,8 +28,8 @@ export async function fetchWithTimeout(url = '/', timeout = 5000) {
     clearTimeout(timeoutId);
     return response;
   } catch (error) {
-    if (error.name === 'AbortError') {
-      console.warn('Request timed out');
+    if (error.name === "AbortError") {
+      console.warn("Request timed out");
       return null;
     }
     throw error;
@@ -37,11 +37,12 @@ export async function fetchWithTimeout(url = '/', timeout = 5000) {
 }
 
 /**
- * @param {Boolean} float Node only
- * @returns {number} Uptime
+ * Node only
+ * @param {Boolean} float If true, returns uptime in seconds as a float. Default: false
+ * @returns {number} Uptime in seconds
  */
 export function uptime(float = false) {
-  if (float) return process.uptime()
+  if (float) return process.uptime();
   return Math.round(process.uptime() * 100000);
 }
 
@@ -66,7 +67,7 @@ export function loop(tps = 60) {
       lastTickTime = currentTime + (deltaTime % tickDuration);
       const start = Date.now();
 
-      await Promise.all(callbacks.map(callback => callback(currentTime)));
+      await Promise.all(callbacks.map((callback) => callback(currentTime)));
 
       const processingTime = Date.now() - start;
 
@@ -108,7 +109,7 @@ export function getHash(splitter) {
   let hash = window.location.hash;
   if (!hash) return null;
   hash = decodeURIComponent(hash);
-  hash = hash.replace('#', '').split(splitter || '&');
+  hash = hash.replace("#", "").split(splitter || "&");
   let obj = {};
   for (var kv = 0; kv < hash.length; kv++) {
     var flag = hash[kv].split("=");
@@ -119,21 +120,19 @@ export function getHash(splitter) {
     }
   }
   return obj;
-
 }
 
 import MobileDetect from "mobile-detect";
-import exp from "constants";
 /**
- * 
+ *
  * @param {string} from data to detect. dafault: window.navigator.userAgent
  * @param {Boolean} more return all detect data
- * @returns 
+ * @returns
  */
 export function isMobile(from = window.navigator.userAgent, more = false) {
   const detect = new MobileDetect(from);
   if (more) return detect;
-  return detect.phone()
+  return detect.phone();
 }
 
 /**
@@ -141,24 +140,54 @@ export function isMobile(from = window.navigator.userAgent, more = false) {
  * @param {Number} ratio_h ratio of height eg: 9
  * @param {Number} w viewport width. default: window.innerWidth
  * @param {Number} h viewport height. default: window.innerHeight
- * @returns 
+ * @returns
  */
-export function AutoRatio(ratio_w, ratio_h, w = window.innerWidth, h = window.innerHeight) {
-  if (h > w / ratio_w * ratio_h)
+export function AutoRatio(
+  ratio_w,
+  ratio_h,
+  w = window.innerWidth,
+  h = window.innerHeight
+) {
+  if (h > (w / ratio_w) * ratio_h)
     return {
       width: w,
-      height: w / ratio_w * ratio_h,
-      oritation: 'h'
-
-    }
+      height: (w / ratio_w) * ratio_h,
+      oritation: "h",
+    };
   return {
-    width: h / ratio_h * ratio_w,
+    width: (h / ratio_h) * ratio_w,
     height: h,
-    oritation: 'w'
-
-  }
+    oritation: "w",
+  };
 }
 
+/**
+ * Checks if the browser is in dark mode.
+ * @returns {boolean} True if dark mode is enabled, false otherwise.
+ */
 export function isDarkMode() {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+}
+
+import ip from "ip";
+/**
+ * Converts a CIDR notation to a list of IP addresses.
+ * @param {string} cidr - CIDR notation (e.g., "192.168.1.1/24")
+ * @returns {string[]} List of IP addresses in the CIDR range.
+ */
+export function CIDR2IP(cidr = "192.168.1.1/24") {
+  const ipList = [];
+
+  const startIP = ip.toLong(ip.cidrSubnet(cidr).firstAddress);
+  const endIP = ip.toLong(ip.cidrSubnet(cidr).lastAddress);
+
+  for (let i = startIP; i <= endIP; i++) {
+    const currentIP = ip.fromLong(i);
+    ipList.push(currentIP);
+  }
+
+  return ipList;
 }
